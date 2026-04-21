9b1346b3ac720f162808c85a49b85c4112f07aab
dielsamaloku3 <dielsa.maloku@student.uni-pr.edu>
2026-04-22 00:32:55 +0200

    Forma e login

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once 'includes/auth.php';
+
+$error = '';
+$login = '';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="scss/registration.css">
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Login</h2>
+
+        <form method="post" action="login.php">
+            <div class="input-box1">
+                <input type="text" name="login" placeholder="Enter your username or email" required>
+            </div>
+
+            <div class="input-box2">
+                <input type="password" name="password" placeholder="Enter your password" required>
+            </div>
+
+            <div class="input-box button">
+                <input type="submit" value="Login Now">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
