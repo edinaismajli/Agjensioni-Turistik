8c7c798d078a8948ef13b21515ffca4b771a8c69
edinaismajli <edina.ismajli@student.uni-pr.edu>
2026-04-26 15:00:24 -0100

    added arrays

--- frontend/book.php
+++ frontend/book.php
@@ -1,6 +1,15 @@
 <?php
 session_start();
 
+$destinations = [
+    "India",
+    "Switzerland",
+    "Latvia",
+    "France",
+    "Japan",
+    "Australia"
+];
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 $name        = trim($_POST["name"]);
@@ -25,12 +34,19 @@ $leaving     = trim($_POST["leaving"]);
     if (!preg_match("/^[0-9]{8,15}$/", $phone)) {
         $errors[] = "Invalid phone number";
     }
-    
+
     if ($guests < 1 || $guests > 20) {
-    $errors[] = "Guests must be between 1 and 20";
-}
+        $errors[] = "Guests must be between 1 and 20";
+    }
 
     if (count($errors) == 0) {
+
+        $userBooking = [
+            "name" => $name,
+            "email" => $email,
+            "destination" => $destination,
+            "guests" => $guests
+        ];
 
         $_SESSION["booking_name"] = $name;
         $_SESSION["booking_email"] = $email;
