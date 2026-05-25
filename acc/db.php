7e60eb90ff8da6d05263d95448d67e2abf5e3555
dielsamaloku <dielsa.maloku@student.uni-pr.edu>
2026-05-25 14:05:47 +0200

    Konfiguro lidhjen me databaze

---
+++ acc/db.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = "127.0.0.1";
+$port = 3306;
+$username = "root";
+$password = "";
+$dbname = "agjensioni-turistik";
+
+$con = mysqli_connect($host, $username, $password, $dbname, $port);
+
+if (!$con) {
+    die("Db connection error..." . mysqli_connect_error());
+}
+
+?>
