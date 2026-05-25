2f2e9abc5e0ce938bb45fa43783d2e0f88c3aa72
dielsamaloku <dielsa.maloku@student.uni-pr.edu>
2026-05-25 15:25:34 +0200

    ndryshime

--- acc/db.php
+++ acc/db.php
@@ -1,15 +1,14 @@
 <?php
-
 $host = "127.0.0.1";
-$port = 3307;
+$port = "3307";
+$dbname = "agjensioni-turistik";
 $username = "root";
 $password = "";
-$dbname = "agjensioni-turistik";
 
-$con = mysqli_connect($host, $username, $password, $dbname, $port);
-
-if (!$con) {
-    die("Db connection error..." . mysqli_connect_error());
+try {
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
 }
-
 ?>
