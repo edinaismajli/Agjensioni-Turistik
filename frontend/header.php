e4261f50838204d3d1e5e5d5326cf6cf07f79381
dielsamaloku <dielsa.maloku@student.uni-pr.edu>
2026-05-26 21:27:16 +0200

    header

--- frontend/header.php
+++ frontend/header.php
@@ -4,11 +4,15 @@ $navItems = [
     ["name" => "Home", "link" => "index.php"],
     ["name" => "Package", "link" => "package.php"],
     ["name" => "Book", "link" => "book1.php"],
-    ["name" => "About", "link" => "about.php"],
-    ["name" => "Login", "link" => "../acc/login.php"],
-    ["name" => "Sign Up", "link" => "../acc/signup.php"],
-    ["name" => "Logout", "link" => "../acc/logout.php", "class" => "logout"]
+    ["name" => "About", "link" => "about.php"]
 ];
+
+if (isset($_SESSION['user_id'])) {
+    $navItems[] = ["name" => "Logout", "link" => "../acc/logout.php", "class" => "logout"];
+} else {
+    $navItems[] = ["name" => "Login", "link" => "../acc/login.php"];
+    $navItems[] = ["name" => "Sign Up", "link" => "../acc/signup.php"];
+}
 
 ?>
 
