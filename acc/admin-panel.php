f3ff3e3c98f4e3ce7fea07675ae43eb095442166
blertalatifi <blerta.latifi4@student.uni-pr.edu>
2026-05-26 13:06:57 +0200

    Remove 'Delete Packages' button from admin panel and update request log with new entry

--- acc/admin-panel.php
+++ acc/admin-panel.php
@@ -56,7 +56,6 @@ $admin = new Admin(
         </div>
 
         <button data-section-id="addPackageSection">Add Package</button>
-        <button data-section-id="managePackagesSection">Delete Packages</button>
         <button data-section-id="bookings">Manage Bookings</button>
          <?php if (isset($_SESSION["role"]) && $_SESSION["role"] === "admin"): ?>
         <button onclick="window.location.href='/Agjensioni-Turistik/frontend/index.php'">Go to Index</button>
